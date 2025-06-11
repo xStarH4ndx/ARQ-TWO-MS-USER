7e5c21e82c5f2de4821343bff30ef67623ef836a
Bruno Toro <146247223+xStarH4ndx@users.noreply.github.com>
2025-06-10 23:49:32 -0400

    conexion rabbit ms payments

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
+import { rabbitmqConfig } from './rabbitmq/rabbitmq.config';
 
 async function bootstrap() {
   dotenv.config();
+
+  const app = await NestFactory.create(AppModule);
+
+  // Rutas de los proto
   const userProtoPath = join(__dirname, '../proto/users.proto');
   const housesProtoPath = join(__dirname, '../proto/houses.proto');
   const authProtoPath = join(__dirname, '../proto/auth.proto');
@@ -14,16 +19,30 @@ async function bootstrap() {
   console.log('Houses Proto Path:', housesProtoPath);
   console.log('Auth Proto Path:', authProtoPath);
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  // Microservicio gRPC
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
-      package: ['users', 'houses', 'auth'], 
+      package: ['users', 'houses', 'auth'],
       protoPath: [userProtoPath, housesProtoPath, authProtoPath],
       url: '0.0.0.0:5000',
     },
   });
-  
-  console.log('🚀 Microservice is listening on port 5000');
-  await app.listen();
+
+  // Microservicio RabbitMQ
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [rabbitmqConfig.uri],
+      queue: rabbitmqConfig.queue,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  // Iniciar ambos microservicios
+  await app.startAllMicroservices();
+  console.log('🚀 Microservice is listening on gRPC (5000) and RabbitMQ');
 }
 bootstrap();
