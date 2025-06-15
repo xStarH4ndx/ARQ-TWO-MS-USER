98d7b6af28f0c2b3549880a4125613434d99efc0
Bruno Toro <146247223+xStarH4ndx@users.noreply.github.com>
2025-06-14 23:56:21 -0400

    bug fix

--- src/houses/houses.module.ts
+++ src/houses/houses.module.ts
@@ -3,12 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { HousesService } from './houses.service';
 import { HousesController } from './houses.controller';
 import { Houses, HousesSchema } from './entities/house.entity';
-import { HouseListener } from './houses.listener';
+import { HousesListener } from './houses.listener';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Houses.name, schema: HousesSchema }])],
   controllers: [HousesController],
-  providers: [HousesService, HouseListener],
+  providers: [HousesService, HousesListener],
   exports: [HousesService],
 })
 export class HousesModule {}
